bd933fcd7857dca328a1a206ebb769939b2bc758
aaronjudd <aaronjudd@me.com>
2014-05-20 09:10:32 -0700

    user add meteor user to host
    setup user prior to Mup setup

--- ec2-ubuntu-data-script.sh
+++ ec2-ubuntu-data-script.sh
@@ -4,6 +4,7 @@ apt-get upgrade -y
 echo DOCKER_OPTS=\"-H tcp://127.0.0.1:4243 -H unix:///var/run/docker.sock\" >> /etc/default/docker.io
 apt-get install -y docker.io
 ln -sf /usr/bin/docker.io /usr/local/bin/docker
+useradd meteoruser
 usermod -aG docker meteoruser
 usermod -aG docker ubuntu
 service docker.io restart
