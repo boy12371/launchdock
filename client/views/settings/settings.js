07b0041b21058c1c2bdb648c6b67968c241cc5e7
aaronjudd <aaronjudd@me.com>
2014-08-03 23:03:01 -0700

    Add email / smtp configuration in settings
    (no emails being sent yet though)

--- client/views/settings/settings.js
+++ client/views/settings/settings.js
@@ -3,14 +3,3 @@ Template.settings.helpers({
     return Settings.findOne();
   }
 });
-
-AutoForm.hooks({
-  settingsAdminForm: {
-    onSuccess: function(operation, result, template) {
-      alertify.success("System settings saved.", "success");
-    },
-    onError: function(operation, error, template) {
-      alertify.error("System settings update failed. " + error, "danger");
-    }
-  }
-});
---
+++ common/3-settings.js
@@ -0,0 +1,38 @@
+// if (!process.env.METEOR_SETTINGS) {
+//   console.log("=> No METEOR_SETTINGS passed in, using locally defined settings.");
+//   Meteor.settings =
+//     {
+//         "public": {
+//             "ga": {
+//                 "account": "***REMOVED***"
+//             }
+//         },
+//         "MailChimpOptions": {
+//             "apiKey": "***REMOVED***",
+//             "listId": "ee1a7dc8a4"
+//         },
+//         "rockerSettings": {
+//             "mongoUser": "admin",
+//             "mongoPassword": "***REMOVED***",
+//             "launchMongoDB": "SG-ReactionDBCluster-2823.servers.mongodirector.com:27017,SG-ReactionDBCluster-2824.servers.mongodirector.com:27017/",
+//             "replicaSet": "RS-ReactionDBCluster-0",
+//             "ddpUrl": "http://54.201.164.44:8080",
+//             "ddpPassword": "***REMOVED***"
+//         }
+//     }
+
+//   // Push a subset of settings to the client.
+//   if (Meteor.settings && Meteor.settings.public) {
+//     __meteor_runtime_config__.PUBLIC_SETTINGS = Meteor.settings.public;
+//   }
+// }
+
+settings = Settings.findOne();
+
+if (settings.kadira) {
+  Kadira.connect(settings.kadira.appId, settings.kadira.appSecret)
+}
+
+if (settings.mailURL) {
+    Meteor.settings.MAIL_URL = settings.mailUrl;
+}
