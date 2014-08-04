5ae47b9e865f179027cdbcf3489cfbb176c6721b
aaronjudd <aaronjudd@me.com>
2014-08-04 07:35:06 -0700

    Updated

---
+++ common/3-settings.js
@@ -0,0 +1,10 @@
+settings = Settings.findOne();
+if (settings) {
+  if (settings.kadira) {
+    Kadira.connect(settings.kadira.appId, settings.kadira.appSecret)
+  }
+
+  if (settings.mailURL) {
+      Meteor.settings.MAIL_URL = settings.mailUrl;
+  }
+}
